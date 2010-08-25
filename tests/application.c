f624c8c7c02821b9559db317f48ce24b261425b5
Jon Nordby <jonn@openismus.com>
2010-08-25 15:06:46 +0200

    Tests: More application tests

--- tests/application.c
+++ tests/application.c
@@ -11,6 +11,9 @@
 
 #include "common.h"
 
+
+int open_files_cb_repeat_count = 10;
+
 /* Forcefully destroy the main window 
  * Workaround because the window of test does not seem to be removed
  * before the window of the last test is */
@@ -23,7 +26,23 @@ massifg_application_destroy(MassifgApplication *app) {
 
 }
 
-/* Callbacks */
+/* Open the same file over and over again. */
+gboolean
+open_files_cb(gpointer data) {
+	MassifgApplication *app = (MassifgApplication *)data;
+	gchar *filename = get_test_file(TEST_INPUT_LONG);
+	static int file_no = 0;
+
+	massifg_application_set_file(app, filename);
+	file_no++;
+	g_free(filename);
+
+	if (file_no == open_files_cb_repeat_count) {
+		return FALSE;
+	}
+	return TRUE;
+}
+
 gboolean
 quit_gtkmain_cb(gpointer data) {
 	gtk_main_quit();
@@ -44,6 +63,20 @@ disable_legend_cb(gpointer data) {
 	return FALSE;
 }
 
+gboolean
+enable_details_cb(gpointer data) {
+	MassifgApplication *app = (MassifgApplication *)data;
+	massifg_graph_set_show_details(app->graph, TRUE);
+	return FALSE;
+}
+
+gboolean
+disable_details_cb(gpointer data) {
+	MassifgApplication *app = (MassifgApplication *)data;
+	massifg_graph_set_show_details(app->graph, FALSE);
+	return FALSE;
+}
+
 /* Tests */
 void
 application_start_quit(void) {
@@ -56,7 +89,7 @@ application_start_quit(void) {
 
 	massifg_application_run(app);
 
-/*	massifg_application_destroy(app);*/
+	massifg_application_destroy(app);
 	massifg_application_free(app);
 	g_usleep(G_USEC_PER_SEC*1);
 
@@ -74,8 +107,45 @@ application_start_open_file(void) {
 
 	massifg_application_run(app);
 
-/* FIXME: Raises a warning that causes the test to fail, even though everything is working as expected */
-/*	massifg_application_destroy(app);*/
+	massifg_application_destroy(app);
+	massifg_application_free(app);
+	g_free(argv[1]);
+	g_usleep(G_USEC_PER_SEC*1);
+
+}
+
+void
+application_open_many_files(void) {
+	int argc = 1;
+	char **argv = g_new(char *, argc);
+	argv[0] = "bin/massifg";
+
+	MassifgApplication *app = massifg_application_new(&argc, &argv);
+	g_timeout_add_seconds(1, open_files_cb, app);
+	g_timeout_add_seconds(open_files_cb_repeat_count+2, quit_gtkmain_cb, NULL);
+
+	massifg_application_run(app);
+
+	massifg_application_destroy(app);
+	massifg_application_free(app);
+	g_usleep(G_USEC_PER_SEC*1);
+}
+
+void
+application_toogle_detailed_view(void) {
+	int argc = 2;
+	char **argv = g_new(char *, argc);
+	argv[0] = "bin/massifg";
+	argv[1] = get_test_file(TEST_INPUT_LONG);
+
+	MassifgApplication *app = massifg_application_new(&argc, &argv);
+	g_timeout_add_seconds(1, enable_details_cb, app);
+	g_timeout_add_seconds(2, disable_details_cb, app);
+	g_timeout_add_seconds(3, quit_gtkmain_cb, NULL);
+
+	massifg_application_run(app);
+
+	massifg_application_destroy(app);
 	massifg_application_free(app);
 	g_free(argv[1]);
 	g_usleep(G_USEC_PER_SEC*1);
@@ -96,8 +166,7 @@ application_toogle_legend(void) {
 
 	massifg_application_run(app);
 
-/* FIXME: Raises a warning that causes the test to fail, even though everything is working as expected */
-/*	massifg_application_destroy(app);*/
+	massifg_application_destroy(app);
 	massifg_application_free(app);
 	g_free(argv[1]);
 	g_usleep(G_USEC_PER_SEC*1);
@@ -118,6 +187,9 @@ main (int argc, char **argv) {
 	g_test_add_func("/application/start-quit", application_start_quit);
 	g_test_add_func("/application/start-open-file", application_start_open_file);
 
+	g_test_add_func("/application/open-many-files", application_open_many_files);
+
+	g_test_add_func("/application/toggle-details", application_toogle_detailed_view);
 	g_test_add_func("/application/toggle-legend", application_toogle_legend);
 
 	massifg_utils_configure_debug_output();
