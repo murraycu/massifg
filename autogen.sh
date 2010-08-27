75455f96ea842c0adda36e90fab82f00dbb85330
Jon Nordby <jonn@openismus.com>
2010-08-27 13:50:15 +0200

    Generate gtk-doc documentation
    Build a temporary library with libtool for gtk-doc to analyze.

--- autogen.sh
+++ autogen.sh
@@ -3,6 +3,9 @@
 test -n "$srcdir" || srcdir=`dirname "$0"`
 test -n "$srcdir" || srcdir=.
 
+gtkdocize --copy --docdir docs --flavour no-tmpl || exit 1
+
 autoreconf --force --install --verbose "$srcdir"
 test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
+
 
